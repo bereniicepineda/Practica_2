e9c2043573df6d966740bc3e802864f511e2b4f6
bereniicepineda <miriam_bereniize7@hotmail.com>
2015-09-21 17:20:03 +0000

    Se cambiaron imagenes, nombres y texto

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,29 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    name: 'Jessica Ledezma',
+    lastText: 'wait a moment',
+    face: 'http://static.ellahoy.es/ellahoy/fotogallery/625X0/147629/vestido-pin-up-negro-vuelo-y-guantes.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    name: 'Caren Ledezma',
+    lastText: 'Im here',
+    face: 'http://img.webdelamoda.com.s3.amazonaws.com/wp-content/uploads/2012/01/modahipster10.jpg'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    name: 'Tania Rodriguez',
+    lastText: 'Hey,call me please',
+    face: 'https://modaddictiondotnet.files.wordpress.com/2012/09/mejor-vestidos-europa-best-clothing-europe-skyscranners-modaddiction-fashion-moda-chic-glamour-hipster-italia-francia-espana-inglaterra-dinamarca-paris-mujer-woman.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    name: 'Arlene Mota',
+    lastText: 'Yes, this is the classroom',
+    face: 'http://i2.esmas.com/galerias/fotos/2012/8/_d-a2354200-36d4-1030-91f2-0019b9d5c8df.jpg'
   }, {
     id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    name: 'Adriana Alvarez',
+    lastText: 'See you later!',
+    face: 'http://24.media.tumblr.com/tumblr_me3vejMVkm1rruy10o1_400.jpg'
   }];
 
   return {
