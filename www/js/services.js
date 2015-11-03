9fd25926fdf3dad0328f77990d437aca7789d380
bereniicepineda <miriam_bereniize7@hotmail.com>
2015-11-03 22:53:15 +0000

    cambiaron archivos hoy 3de noviembre

--- www/js/services.js
+++ www/js/services.js
@@ -1,36 +1,5 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Jessica Ledezma',
-    lastText: 'wait a moment',
-    face: 'http://static.ellahoy.es/ellahoy/fotogallery/625X0/147629/vestido-pin-up-negro-vuelo-y-guantes.jpg'
-  }, {
-    id: 1,
-    name: 'Caren Ledezma',
-    lastText: 'Im here',
-    face: 'http://img.webdelamoda.com.s3.amazonaws.com/wp-content/uploads/2012/01/modahipster10.jpg'
-  }, {
-    id: 2,
-    name: 'Tania Rodriguez',
-    lastText: 'Hey,call me please',
-    face: 'https://modaddictiondotnet.files.wordpress.com/2012/09/mejor-vestidos-europa-best-clothing-europe-skyscranners-modaddiction-fashion-moda-chic-glamour-hipster-italia-francia-espana-inglaterra-dinamarca-paris-mujer-woman.jpg'
-  }, {
-    id: 3,
-    name: 'Arlene Mota',
-    lastText: 'Yes, this is the classroom',
-    face: 'http://i2.esmas.com/galerias/fotos/2012/8/_d-a2354200-36d4-1030-91f2-0019b9d5c8df.jpg'
-  }, {
-    id: 4,
-    name: 'Adriana Alvarez',
-    lastText: 'See you later!',
-    face: 'http://24.media.tumblr.com/tumblr_me3vejMVkm1rruy10o1_400.jpg'
-  }];
-
   return {
     all: function() {
       return chats;
