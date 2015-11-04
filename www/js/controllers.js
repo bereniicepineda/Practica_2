27b7564fd8b45af10c1e5f447d40598b0e9d9f86
bereniicepineda <miriam_bereniize7@hotmail.com>
2015-11-04 18:24:50 +0000

    cambio archivo controllers js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,45 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $ionicPopup, $timeout,$cordovaSQLite) {
-    
-    
+.controller('DashCtrl', function($scope,$ionicPopup, $timeout,$cordovaSQLite) {
+
     $scope.showAlert = function() {
      var alertPopup = $ionicPopup.alert({
        title: 'Agenda',
        template: 'Datos guardados'
      });
     }
-    
-    $scope.guardar = function(persona)
-{
-    
- 
-    $cordovaSQLite.execute(db, 'INSERT INTO agenda (nombre,apellido,telefono,email) VALUES (?,?,?,?)', [persona.nombre,persona.apellido,persona.telefono,persona.email])
+
+    $scope.guardar = function(persona){
+
+        $cordovaSQLite.execute(db, 'INSERT INTO agenda (nombre,apellido,telefono,email) VALUES (?,?,?,?)', [persona.nombre,persona.apellido,persona.telefono,persona.email])
         .then(function(result) {
             $scope.statusMessage = "Registro guardado!";
         }, function(error) {
             $scope.statusMessage = "Error al guardar: " + error.message;
         })
-    
-    
-    
-    
-    
-    console.log("Nombre: " +persona.nombre);
-     console.log("Apellido: " +persona.apellido);
-      console.log("Telefono: " +persona.telefono);
-       console.log("Email: " +persona.email);
-}
-    
-    
-    
-    
+
+        /*
+        console.log("Nombre: "+persona.nombre);
+        console.log("Apellido: "+persona.apellido);
+        console.log("Telefono: "+persona.telefono);
+        console.log("Email: "+persona.email);
+        */
+    }
+
 })
-
-
-
-
-
 
 .controller('ChatsCtrl', function($scope, Chats,$cordovaSQLite) {
   // With the new view caching in Ionic, Controllers are only called
@@ -50,38 +37,44 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = [];
-  
-   $cordovaSQLite.execute(db, 'SELECT * FROM agenda ORDER BY id DESC')
-            .then(
-                function(result) {
- 
-                    if (result.rows.length > 0) {
-                        for(var i=0;i< result.rows.length;i++)
-                        {
-                            $scope.chats.push({"nombre":result.rows.item(i).nombre,
-                                                "apellido":result.rows.item(i).apellido,
-                                                "telefono":result.rows.item(i).telefono,
-                                                "email":result.rows.item(i).email
-                            });
-                        }
-                    
+  $scope.chats = Chats.all();
+  //$scope.chats = [];
+
+  /*
+  $cordovaSQLite.execute(db, 'SELECT * FROM agenda ORDER BY id DESC')
+       .then(
+          function(result) {
+             if (result.rows.length > 0) {
+                      for(var i = 0; i < result.rows.length; i++)
+                      {
+                        $scope.chats.push({"nombre":result.rows.item(i).nombre,
+                                    "apellido":result.rows.item(i).apellido,
+                                    "telefono":result.rows.item(i).telefono,
+                                    "email":result.rows.item(i).email});
+                      }
                     }
                 },
                 function(error) {
                     $scope.statusMessage = "Error on loading: " + error.message;
                 }
-            );
-  
-  
-  
+        );
+  */
+
+  $scope.getAll = function()
+  {
+      $scope.chats = Chats.all();
+  };
+
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  //alert($stateParams.chatId);
+  //alert(Chats.get($stateParams.chatId));
   $scope.chat = Chats.get($stateParams.chatId);
+
 })
 
 .controller('AccountCtrl', function($scope) {
